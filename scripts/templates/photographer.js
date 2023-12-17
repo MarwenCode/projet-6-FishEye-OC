da093d25ae1e135633e1f88b97e03f0f9fb3ccca
MarwenCode <marwen_jdidi@yahoo.com>
2023-12-18 00:12:32 +0100

    display picture on lightbow with arrows

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -9,7 +9,6 @@ export function photographerTemplate(data) {
   const article = document.createElement("article");
 
   function getUserCardDOM() {
-    
     const img = document.createElement("img");
     img.classList.add("pic");
     img.setAttribute("src", picture);
@@ -41,27 +40,34 @@ export function photographerTemplate(data) {
     return article;
   }
 
-  const displayMedia = (photographerMedia,mediaContainer,encartLikesContainer,photographerPrice) => {
+  const displayMedia = (
+    photographerMedia,
+    mediaContainer,
+    encartLikesContainer,
+    photographerPrice
+  ) => {
     photographerMedia.forEach((media) => {
       const mediaFactory = new MediaFactory(media, media.photographerId);
       mediaContainer.innerHTML += mediaFactory.renderMedia();
     });
 
     const encartLikes = new MediaFactory(photographerMedia);
-    encartLikesContainer.innerHTML = encartLikes.renderEncart(photographerPrice);
+    encartLikesContainer.innerHTML =
+      encartLikes.renderEncart(photographerPrice);
 
+    // Add the event listener for lightbox
+    mediaContainer.addEventListener("click", (event) => {
+      const clickedElement = event.target.closest(".media-element");
+      console.log(clickedElement);
+      console.log(clickedElement.parentNode.children);
 
-      // Add the event listener for lightbox
-      mediaContainer.addEventListener("click", (event) => {
-        const clickedElement = event.target.closest('.media-element');
-        console.log(clickedElement)
-        console.log(clickedElement.parentNode.children)
-  
-        if (clickedElement) {
-          const index = Array.from(clickedElement.parentNode.children).indexOf(clickedElement);
-          showLightBox(index, photographerMedia);
-        }
-      })
+      if (clickedElement) {
+        const index = Array.from(clickedElement.parentNode.children).indexOf(
+          clickedElement
+        );
+        showLightBox(index, photographerMedia);
+      }
+    });
   };
 
   //contact form
@@ -113,189 +119,107 @@ export function photographerTemplate(data) {
 
       closeModal();
     });
-
-   
   };
 
   submitForm();
-
 
   ///show LightBox
 
   const showLightBox = (index, photographerMedia) => {
     // Get the clicked media
     const clickedMedia = photographerMedia[index];
-  
-  
+
     console.log("User clicked on the image:", clickedMedia);
 
-    createLightBox()
-  
+    createLightBox(clickedMedia);
+
     // You can add more functionality here, such as opening a modal or displaying the image in a lightbox
-
-  
-
   };
-   
-  
-    return { name, picture, getUserCardDOM, displayMedia, showLightBox };
-  }
-  
 
-  //create LightBox
+  return { name, picture, getUserCardDOM, displayMedia, showLightBox };
+}
 
-  const createLightBox = () => {
-    // Create modal elements
-    let imgCloseBtn = document.createElement("img");
-    let modalContact = document.createElement("div");
-    let modal = document.createElement("div");
-    let header = document.createElement("header");
-  
-    // Add classes to modal elements
-    modalContact.classList.add("modal_contact");
-    modal.classList.add("modal");
-    header.classList.add("header");
-    imgCloseBtn.classList.add("close");
-  
-    // Set styles for modalContact
-    modalContact.style.position = "fixed";
-    modalContact.style.top = "0";
-    modalContact.style.left = "0";
-    modalContact.style.width = "100%";
-    modalContact.style.height = "100%";
-    modalContact.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
-  
-    // Set styles for modal
-    modal.style.width = "550px";
-    modal.style.height = "550px";
-    modal.style.margin = "auto";
-    // modal.style.marginTop = "20px";
-    modal.style.marginBottom = "60px";
-    modal.style.backgroundColor = "red";
-  
- // Set styles for close button
- // Set styles for close button
- imgCloseBtn.src = "assets/icons/close.svg";
- imgCloseBtn.alt = "Close";
- imgCloseBtn.style.cursor = "pointer";
- imgCloseBtn.style.position = "absolute";
- imgCloseBtn.style.top = "10px"; // Adjust the top position if needed
- imgCloseBtn.style.right = "10px"; // Adjust the right position if needed
- imgCloseBtn.style.backgroundColor = "red"; // Change the background color to red
-    
-  
-    // Append modal elements
-    header.append(imgCloseBtn);
-    modalContact.append(header, modal);
+//create LightBox
 
-  
-    // Show the lightbox
-    document.body.appendChild(modalContact);
-  
-    // Add event listener to close the lightbox when the close button is clicked
-    imgCloseBtn.addEventListener("click", () => {
-      modalContact.style.display = "none";
-    });
-  };
-  
-  
+const createLightBox = (clickedMedia) => {
+  // Create modal elements
+  let imgCloseBtn = document.createElement("img");
+  let modalContact = document.createElement("div");
+  let modal = document.createElement("div");
+  let header = document.createElement("header");
+  let content = document.createElement("div")
+  let media = document.createElement("img");
+
+  let nextArrow = document.createElement("span");
+  let backArrow = document.createElement("span");
+
+  // Add classes to modal elements
+  modalContact.classList.add("modal_contact");
+  content.classList.add("content")
+  modal.classList.add("modal");
+  media.classList.add("media");
+
+  nextArrow.classList.add("nextArrow");
+  backArrow.classList.add("backArrow");
+
+  media.src = `../../assets/photos/${clickedMedia.photographerId}/${clickedMedia.image}`;
+  console.log(media.src);
+  header.classList.add("header");
+  imgCloseBtn.classList.add("close");
+
+  // Set styles for modalContact
+  modalContact.style.position = "fixed";
+  modalContact.style.top = "0";
+  modalContact.style.left = "0";
+  modalContact.style.width = "100%";
+  modalContact.style.height = "100%";
+  modalContact.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
 
 
-// const createLightBox = (media) => {
-//   // Create the lightbox elements
-//   const contentLightBox = document.createElement("div");
-//   contentLightBox.classList.add("modal");
-  
-//   const headerLightBox = document.createElement("div");
-//   headerLightBox.classList.add("header");
+  // Set styles for modal
+  modal.style.width = "550px";
+  modal.style.height = "550px";
+  modal.style.margin = "auto";
+  // modal.style.marginTop = "20px";
+  modal.style.marginBottom = "60px";
+  // modal.style.backgroundColor = "red";
+  modal.style.backgroundColor = "white";
+  media.style.width ="450px";
+  media.style.height ="100%";
 
-//   const closeBtn = document.createElement("img");
-//   closeBtn.src = "./assets/icons/close.svg";
-//   closeBtn.alt = "close button";
-//   closeBtn.classList.add("close");
-//   closeBtn.addEventListener("click", () => {
-//     // Close the lightbox when the close button is clicked
-//     contentLightBox.style.display = "none";
-//   });
-
-//   headerLightBox.appendChild(closeBtn);
-//   contentLightBox.appendChild(headerLightBox);
-
-//   // Implement the rest of your lightbox content based on the 'media' object
-//   // For example, you can add an image or video element to display the media content
-//   const mediaElement = document.createElement("img");
-//   // const mediaElement = document.createElement(media.video ? "video" : "img");
-//   // mediaElement.src = media.video ? `path_to_videos/${media.video}` : `path_to_images/${media.image}`;
-//   // mediaElement.controls = true; // Add controls for video
-
-//   contentLightBox.appendChild(mediaElement);
-
-//   // Append the lightbox to the document body
-//   document.body.appendChild(contentLightBox);
-
-//   // Show the lightbox
-//   contentLightBox.style.display = "block";
-// };
+  // Set styles for close button
+  // Set styles for close button
+  imgCloseBtn.src = "assets/icons/close.svg";
+  imgCloseBtn.alt = "Close";
+  imgCloseBtn.style.cursor = "pointer";
+  imgCloseBtn.style.position = "absolute";
+  imgCloseBtn.style.top = "10px"; // Adjust the top position if needed
+  imgCloseBtn.style.right = "10px"; // Adjust the right position if needed
+  imgCloseBtn.style.backgroundColor = "red"; // Change the background color to red
 
 
+  //arrows
+  // Set styles for arrows
+  nextArrow.textContent = "➡️";
+  backArrow.textContent = "⬅️";
+  nextArrow.style.position = "absolute";
+  // nextArrow.style.top = "50%";
+  nextArrow.style.right = "450px";
+  backArrow.style.position = "absolute";
+  // backArrow.style.top = "50%";
+  backArrow.style.left = "180px";
+  // Append modal elements
+  header.append(imgCloseBtn);
+  content.append(media, nextArrow, backArrow)
+  modal.append(content);
+  modalContact.append(header, modal);
 
+  // Show the lightbox
+  document.body.appendChild(modalContact);
 
+  // Add event listener to close the lightbox when the close button is clicked
+  imgCloseBtn.addEventListener("click", () => {
+    modalContact.style.display = "none";
+  });
+};
 
-//LightBox pictures
-//   const showLightBox = (media) => {
-   
-
-//     const contentLightBox = document.createElement("div");
-//     contentLightBox.classList.add("modal");
-//     const headerLightBox = document.createElement("div");
-//     headerLightBox.classList.add("header")
-
-//     const closeBtn = document.querySelector("img");
-//     closeBtn.classList.add("close");
-    
-//     headerLightBox.append(closeBtn)
-//     contentLightBox.append(headerLightBox)
-
-  
-// // Create the lightbox media container
-// const mediaContainer = document.createElement("div");
-// mediaContainer.classList.add("lightbox-media");
-
-// // Create the selected media element
-// const selectedMedia = document.createElement("div");
-// selectedMedia.classList.add("selected-media");
-
-
-// // Append the selected media to the lightbox container
-// mediaContainer.appendChild(selectedMedia);
-
-// // Append the media container to the lightbox
-// contentLightBox.appendChild(mediaContainer);
-
-// // Append the lightbox to the body
-// document.body.appendChild(contentLightBox);
-
-
-//   }
-
- // lightboxToggler.classList.add("active");
-    // overlay.classList.add("active");
-    // lightbox.src = media;
-    // };
-    // const handleKeyPress = (e) => {
-    //   if(e.key == 'Escape'){
-    //     closeLightBox()
-    //     }
-    //     }
-    //     const closeLightBox = () => {
-    //       lightboxToggler.classList.remove("active");
-    //       overlay.classList.remove("active");
-    //       lightbox.style.opacity=0;
-    //       setTimeout(()=>{lightbox.style.display="none"},500)
-    //       }
-    //       images.forEach((img)=>{
-    //         img.addEventListener('click',(e)=>showLightBox(e.target.dataset.source))
-    //         })
-    //         document.body.addEventListener('keydown',handleKeyPress)
-    //         lightboxToggler.addEventListener('click',closeLightBox)
-            // Scroll Suiveur
